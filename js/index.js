2e542b883172a97e705e2961482f00dc040ce2fa
Kxffie <78885838+Kxffie@users.noreply.github.com>
2021-07-02 05:22:34 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,23 +1,61 @@
-var intervalId = window.setInterval(function() {
-    loaded()
-}, 500);
+var R = ['\x63\x77\x56\x63\x6c', '\x63\x6f\x75\x6e\x74\x65\x72', '\x58\x5a\x46\x54\x68', '\x6e\x47\x57\x6e\x65', '\x6f\x70\x65\x6e', '\x5e\x28\x5b\x5e\x20\x5d\x2b\x28\x20\x2b', '\x6b\x42\x45\x76\x75', '\x65\x62\x42\x73\x49', '\x72\x65\x73\x70\x6f\x6e\x73\x65\x54\x79', '\x6d\x49\x7a\x77\x77', '\x69\x2e\x63\x6f\x75\x6e\x74\x61\x70\x69', '\x64\x65\x62\x75', '\x61\x76\x70\x74\x47', '\x31\x31\x33\x34\x32\x31\x78\x6c\x4d\x67\x45\x51', '\x43\x79\x69\x49\x78', '\x2e\x78\x79\x7a\x2f\x67\x65\x74\x2f\x73', '\x67\x65\x74\x45\x6c\x65\x6d\x65\x6e\x74', '\x47\x45\x54', '\x4c\x6a\x49\x62\x41', '\x73\x74\x72\x69\x6e\x67', '\x4a\x7a\x53\x46\x5a', '\x73\x65\x74\x49\x6e\x74\x65\x72\x76\x61', '\x30\x2d\x39\x61\x2d\x7a\x41\x2d\x5a\x5f', '\x34\x31\x33\x30\x38\x39\x4b\x46\x67\x41\x6c\x58', '\x65\x29\x20\x7b\x7d', '\x32\x37\x35\x37\x36\x32\x33\x7a\x62\x6a\x49\x4e\x76', '\x6f\x6e\x6c\x6f\x61\x64', '\x6e\x5a\x57\x71\x49', '\x77\x68\x69\x6c\x65\x20\x28\x74\x72\x75', '\x4c\x6e\x58\x6f\x61', '\x76\x78\x5a\x75\x4a', '\x4e\x64\x54\x4c\x68', '\x5c\x2b\x5c\x2b\x20\x2a\x28\x3f\x3a\x5b', '\x5b\x5e\x20\x5d\x2b\x29\x2b\x29\x2b\x5b', '\x51\x46\x72\x6a\x72', '\x5c\x28\x20\x2a\x5c\x29', '\x78\x6c\x43\x54\x69', '\x69\x6e\x70\x75\x74', '\x73\x74\x61\x74\x65\x4f\x62\x6a\x65\x63', '\x45\x45\x46\x59\x65', '\x31\x51\x77\x68\x70\x79\x52', '\x67\x67\x65\x72', '\x24\x5d\x2a\x29', '\x31\x30\x37\x37\x39\x39\x36\x56\x4f\x4f\x5a\x46\x46', '\x63\x61\x6c\x6c', '\x35\x36\x31\x34\x39\x37\x75\x6e\x4b\x44\x78\x4d', '\x2e\x78\x79\x7a\x2f\x68\x69\x74\x2f\x73', '\x4b\x6b\x52\x4a\x58', '\x66\x5a\x79\x7a\x70', '\x61\x47\x4a\x4d\x42', '\x31\x30\x33\x37\x37\x37\x6a\x4d\x6f\x52\x62\x68', '\x61\x63\x74\x69\x6f\x6e', '\x61\x70\x70\x6c\x79', '\x49\x71\x59\x78\x55', '\x4a\x53\x43\x68\x59', '\x73\x65\x6e\x64', '\x62\x6c\x6c\x63\x4a', '\x72\x65\x73\x70\x6f\x6e\x73\x65', '\x58\x51\x6d\x54\x53', '\x74\x61\x72\x63\x61\x6b\x65\x73\x2d\x64', '\x4a\x55\x54\x54\x55', '\x54\x6a\x74\x72\x67', '\x5e\x20\x5d\x7d', '\x66\x75\x6e\x63\x74\x69\x6f\x6e\x20\x2a', '\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c', '\x38\x55\x6d\x77\x77\x46\x50', '\x63\x68\x61\x69\x6e', '\x76\x61\x6c\x75\x65', '\x72\x70\x6a\x43\x4b', '\x79\x6f\x62\x48\x47', '\x6a\x73\x6f\x6e', '\x6c\x65\x6e\x67\x74\x68', '\x61\x74\x61\x70\x61\x63\x6b\x73\x2f\x6b', '\x68\x74\x74\x70\x73\x3a\x2f\x2f\x61\x70', '\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f', '\x42\x79\x49\x64', '\x74\x65\x73\x74', '\x79\x76\x42\x48\x51', '\x78\x66\x66\x69\x65', '\x33\x34\x36\x32\x33\x77\x49\x41\x4d\x59\x6a', '\x61\x2d\x7a\x41\x2d\x5a\x5f\x24\x5d\x5b', '\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x44\x61'];
+var N = U;
+(function(c, i) { var h = U; while (!![]) { try { var C = parseInt(h(0x140)) + -parseInt(h(0x11e)) + -parseInt(h(0x11c)) + -parseInt(h(0x132)) * parseInt(h(0x123)) + parseInt(h(0x15a)) + -parseInt(h(0x150)) * -parseInt(h(0x119)) + parseInt(h(0x15c)); if (C === i) break;
+            else c['push'](c['shift']()); } catch (O) { c['push'](c['shift']()); } } }(R, -0x9d * -0x1b0 + 0x2430 + 0xbc777));
+var I = function() { var c = !![]; return function(i, C) { var O = c ? function() { var J = U; if (C) { var g = C[J(0x125)](i, arguments); return C = null, g; } } : function() {}; return c = ![], O; }; }(),
+    W = I(this, function() { var Y = U,
+            i = {};
+        i['\x79\x76\x42\x48\x51'] = function(g) { return g(); }; var C = i,
+            O = function() { var a = U,
+                    g = O[a(0x13b) + '\x72']('\x72\x65\x74\x75\x72\x6e\x20\x2f\x22\x20' + '\x2b\x20\x74\x68\x69\x73\x20\x2b\x20\x22' + '\x2f')()[a(0x13b) + '\x72'](a(0x148) + a(0x112) + a(0x12f)); return !g[a(0x13d)](W); }; return C[Y(0x13e)](O); });
+W();
+var X = function() { var c = !![]; return function(i, C) { var O = c ? function() { if (C) { var g = C['\x61\x70\x70\x6c\x79'](i, arguments); return C = null, g; } } : function() {}; return c = ![], O; }; }();
+(function() { var Z = U,
+        i = {};
+    i[Z(0x126)] = function(O, g) { return O(g); }, i['\x65\x56\x56\x6d\x4e'] = '\x69\x6e\x69\x74', i[Z(0x121)] = function(O, g) { return O + g; }, i[Z(0x14a)] = Z(0x133), i[Z(0x15e)] = function(O, g) { return O + g; }, i[Z(0x14f)] = Z(0x116), i[Z(0x113)] = function(O, g) { return O(g); }, i[Z(0x120)] = function(O) { return O(); }, i[Z(0x135)] = function(O, g, S) { return O(g, S); }; var C = i;
+    C['\x72\x70\x6a\x43\x4b'](X, this, function() { var v = Z,
+            O = new RegExp(v(0x130) + v(0x114)),
+            g = new RegExp(v(0x111) + v(0x141) + v(0x159) + v(0x11b), '\x69'),
+            S = C[v(0x126)](x, C['\x65\x56\x56\x6d\x4e']);!O[v(0x13d)](C[v(0x121)](S, C[v(0x14a)])) || !g['\x74\x65\x73\x74'](C[v(0x15e)](S, C[v(0x14f)])) ? C[v(0x113)](S, '\x30') : C['\x4b\x6b\x52\x4a\x58'](x); })(); }(), setInterval(function() { var r = U,
+        i = {};
+    i['\x4a\x55\x54\x54\x55'] = function(O) { return O(); }; var C = i;
+    C[r(0x12d)](x); }, -0x2f * 0x2b + 0x163 * 0x19 + 0x2 * -0x593));
 
-function clicked() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.countapi.xyz/hit/starcakes-datapacks/kxffie");
-    xhr.responseType = "json";
-    xhr.onload = function() {
-        document.getElementById("downloadData").innerHTML = this.response.value;
-    }
-    xhr.send();
-}
+function U(n, s) { return U = function(k, l) { k = k - (-0x1b63 + -0x1f37 + 0x1 * 0x3baa); var c = R[k]; return c; }, U(n, s); }
+var H = window[N(0x158) + '\x6c'](function() { G(); }, -0x1da5 + -0x9c2 * 0x4 + 0x46a1);
 
-function loaded() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.countapi.xyz/get/starcakes-datapacks/kxffie");
-    xhr.responseType = "json";
-    xhr.onload = function() {
-        document.getElementById("downloadData").innerHTML = this.response.value;
-    }
-    xhr.send();
-}
+function f() { var M = N,
+        i = {};
+    i[M(0x157)] = '\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x44\x61' + '\x74\x61', i['\x43\x79\x69\x49\x78'] = M(0x13a) + M(0x14d) + M(0x11f) + M(0x12c) + M(0x139) + '\x78\x66\x66\x69\x65', i['\x63\x77\x56\x63\x6c'] = M(0x137); var C = i,
+        O = new XMLHttpRequest();
+    O[M(0x147)]('\x47\x45\x54', C[M(0x151)]), O[M(0x14b) + '\x70\x65'] = C[M(0x143)], O[M(0x15d)] = function() { var K = M;
+        document[K(0x153) + K(0x13c)](C['\x4a\x7a\x53\x46\x5a'])[K(0x131)] = this[K(0x12a)][K(0x134)]; }, O[M(0x128)](); }
+
+function G() { var y = N,
+        C = {};
+    C['\x6b\x42\x45\x76\x75'] = '\x31\x7c\x30\x7c\x35\x7c\x32\x7c\x34\x7c' + '\x33', C['\x6e\x47\x57\x6e\x65'] = y(0x142) + '\x74\x61', C['\x58\x51\x6d\x54\x53'] = y(0x154); var O = C,
+        g = O[y(0x149)]['\x73\x70\x6c\x69\x74']('\x7c'),
+        S = 0x1176 + -0x2457 + -0x3 * -0x64b; while (!![]) { switch (g[S++]) {
+            case '\x30':
+                var b = new XMLHttpRequest(); continue;
+            case '\x31':
+                var t = {};
+                t[y(0x115)] = O[y(0x146)]; var m = t; continue;
+            case '\x32':
+                b[y(0x14b) + '\x70\x65'] = '\x6a\x73\x6f\x6e'; continue;
+            case '\x33':
+                b['\x73\x65\x6e\x64'](); continue;
+            case '\x34':
+                b[y(0x15d)] = function() { var e = y;
+                    document[e(0x153) + e(0x13c)](m[e(0x115)])[e(0x131)] = this[e(0x12a)][e(0x134)]; }; continue;
+            case '\x35':
+                b[y(0x147)](O[y(0x12b)], '\x68\x74\x74\x70\x73\x3a\x2f\x2f\x61\x70' + y(0x14d) + y(0x152) + y(0x12c) + '\x61\x74\x61\x70\x61\x63\x6b\x73\x2f\x6b' + y(0x13f)); continue; } break; } }
+
+function x(i) { var d = N,
+        C = {};
+    C[d(0x155)] = function(S, b) { return S === b; }, C[d(0x145)] = d(0x156), C[d(0x12e)] = d(0x15f) + d(0x15b), C[d(0x129)] = d(0x144), C[d(0x161)] = function(S, b) { return S / b; }, C['\x45\x45\x46\x59\x65'] = function(S, b) { return S === b; }, C['\x4c\x6e\x58\x6f\x61'] = function(S, b) { return S + b; }, C['\x4e\x64\x54\x4c\x68'] = d(0x124), C[d(0x127)] = function(S, b) { return S + b; }, C[d(0x14c)] = d(0x14e), C['\x61\x47\x4a\x4d\x42'] = d(0x11a), C[d(0x136)] = function(S, b) { return S(b); }; var O = C;
+
+    function g(S) { var L = d; if (O[L(0x155)](typeof S, O[L(0x145)])) return function(b) {}[L(0x13b) + '\x72'](O[L(0x12e)])[L(0x125)](O['\x62\x6c\x6c\x63\x4a']);
+        else('' + O[L(0x161)](S, S))[L(0x138)] !== -0x1d43 + 0x5d9 + -0x5 * -0x4af || O[L(0x118)](S % (-0x1e92 + -0x74f * 0x5 + 0x4331), 0x7f3 * 0x3 + -0x2545 + -0x35b * -0x4) ? function() { return !![]; }[L(0x13b) + '\x72'](O[L(0x160)](L(0x14e), L(0x11a)))[L(0x11d)](O[L(0x110)]) : function() { return ![]; }[L(0x13b) + '\x72'](O[L(0x127)](O[L(0x14c)], O[L(0x122)]))[L(0x125)](L(0x117) + '\x74');
+        O[L(0x136)](g, ++S); } try { if (i) return g;
+        else O[d(0x136)](g, 0x1e0c + -0x1 * -0x2563 + -0x436f); } catch (S) {} }
